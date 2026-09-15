11f21c5798c087b969d5dc0a3ff35b74159e89d0
lars <s1147979@student.hsleiden.nl>
2026-09-15 13:30:18 +0200

    added second servo controls

--- servo_controller.py
+++ servo_controller.py
@@ -2,27 +2,88 @@
 import pigpio
 import time
 
-servo = 23
-
-# more info at http://abyz.me.uk/rpi/pigpio/python.html#set_servo_pulsewidth
+servoX = 22  # GPIO 22
+servoY = 23  # GPIO 23
 
 pwm = pigpio.pi()
-pwm.set_mode(servo, pigpio.OUTPUT)
 
-pwm.set_PWM_frequency( servo, 50 )
+if not pwm.connected:
+    print("Kan geen verbinding maken met pigpio!")
+    exit()
 
-print( "0 deg" )
-pwm.set_servo_pulsewidth( servo, 500 ) ;
-time.sleep( 3 )
+pwm.set_mode(servoX, pigpio.OUTPUT)
+pwm.set_mode(servoY, pigpio.OUTPUT)
 
-print( "90 deg" )
-pwm.set_servo_pulsewidth( servo, 1500 ) ;
-time.sleep( 3 )
 
-print( "180 deg" )
-pwm.set_servo_pulsewidth( servo, 2500 ) ;
-time.sleep( 3 )
+def servo_off(servo):
+    pwm.set_servo_pulsewidth(servo, 0)
+    pwm.set_PWM_dutycycle(servo, 0)
+    pwm.set_PWM_frequency(servo, 0)
 
-# turning off servo
-pwm.set_PWM_dutycycle( servo, 0 )
-pwm.set_PWM_frequency( servo, 0 )
+
+def servo_on(servo, pulsewidth):
+    # first we turn off both servo's
+    servo_off(servoX)
+    servo_off(servoY)
+
+    # turn on requested servo
+    pwm.set_PWM_frequency(servo, 50)
+    pwm.set_servo_pulsewidth(servo, pulsewidth)
+
+
+# ==========================
+# SERVO Y
+# ==========================
+
+print("Servo Y - 0 graden")
+servo_on(servoY, 500)
+time.sleep(3)
+
+print("Servo Y - 90 graden")
+servo_on(servoY, 1500)
+time.sleep(3)
+
+print("Servo Y - 180 graden")
+servo_on(servoY, 2500)
+time.sleep(3)
+
+servo_off(servoY)
+
+
+# ==========================
+# SERVO X
+# ==========================
+
+print("Servo X - 0 graden")
+servo_on(servoX, 500)
+time.sleep(3)
+
+print("Servo X - 90 graden")
+servo_on(servoX, 1500)
+time.sleep(3)
+
+print("Servo X - 180 graden")
+servo_on(servoX, 2500)
+time.sleep(3)
+
+servo_off(servoX)
+
+
+# ==========================
+# RESET
+# ==========================
+
+print("Reset: Servo X naar 90 graden")
+servo_on(servoX, 1500)
+time.sleep(1)
+servo_off(servoX)
+
+print("Reset: Servo Y naar 90 graden")
+servo_on(servoY, 1500)
+time.sleep(1)
+servo_off(servoY)
+
+print("Beide servo's staan op 90 graden.")
+
+# pigpio afsluiten
+pwm.stop()
